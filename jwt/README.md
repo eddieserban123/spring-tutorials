9a015bb422a7d27cde0de3adf3e405a68704a8d0
eduards <eduards@adobe.com>
2024-08-13 23:07:24 +0300

    add more data

--- jwt/README.md
+++ jwt/README.md
@@ -1,3 +1,18 @@
 https://www.youtube.com/watch?v=7Q17ubqLfaM
 
 https://wannabedev.io/guides/everything-you-need-to-know-about-json-web-token
+
+to generate public private keys
+- generate a pair pf private-public key --> openssl genrsa -out keypair.pem 2048
+- obtain public key from private key   --> openssl rsa -in keypair.pem -pubout -out public.pem
+- transform pem to pkcs8 format         --> openssl pkcs8 -topk8 -inform PEM  -outform  PEM -nocrypt -in keypair.pem  -out private.pem
+
+These public , private keys are going to be read with spring boot configuration processor
+
+curl -u "titi:password" -XPOST  http://localhost:8080/token -v
+
+get token
+curl -H "Authorization: Bearer <Token>" -XGET  http://localhost:8080 -v
+
+![img_1.png](img_1.png)
+
--- jwt/src/main/java/com/example/config/Security.java
+++ jwt/src/main/java/com/example/config/Security.java
@@ -1,5 +1,11 @@
 package com.example.config;
 
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,8 +15,14 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.security.interfaces.RSAPrivateKey;
 
 import static org.springframework.security.config.Customizer.*;
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -18,6 +30,12 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 @EnableWebSecurity
 public class Security {
+
+    private final RsaKeyProperties rsaKeys;
+
+    public Security(RsaKeyProperties rsaKeys) {
+        this.rsaKeys = rsaKeys;
+    }
 
     @Bean
     InMemoryUserDetailsManager user() {
@@ -38,6 +56,18 @@ public class Security {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))  //disable session management
                 .httpBasic(withDefaults())
                 .build();
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withPublicKey(rsaKeys.publicKey()).build();
+    }
+
+    @Bean
+    JwtEncoder jwtEncoder() {
+        JWK jwk = new RSAKey.Builder(rsaKeys.publicKey()).privateKey(rsaKeys.privateKey()).build();
+        JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+        return new NimbusJwtEncoder(jwks);
     }
 
 }
